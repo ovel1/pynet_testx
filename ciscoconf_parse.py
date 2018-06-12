882a2fe6583ce2a1c49b5515cb48dc2cd420c571
Oleg Shumskiy <Oleg.Shumskiy@alphawest.com.au>
2018-06-12 17:58:28 +1000

    First version of cisco_conf_parse

---
+++ ciscoconf_parse.py
@@ -0,0 +1,8 @@
+from ciscoconfparse import CiscoConfParse
+
+cisco_cfg = CiscoConfParse("cisco_configs/adelaide-r01.txt")
+intf=cisco_cfg.find_objects(r"^interface")
+
+for iface in intf:
+    print(iface)    
+
